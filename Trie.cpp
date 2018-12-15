c1746addef74e7e02c8fbf6df66f3d7697ac0735
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-16 00:28:46 +0530

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -7,94 +7,70 @@ searchword(root, s); -> boolean
 */
 
 
-struct node
-{
+struct node {
 	struct node *daughters[26];
 	char ch;
 	bool fin;
 };
 
-struct node *makenode()
-{
+struct node *makenode() {
 	struct node *fnode = new node;
-	fnode->fin = 0;
+	fnode->fin = false;
 	fnode->ch = '#';
-	for(int i = 0; i < 26; i++)
-	{
+	for(int i = 0; i < 26; i++) {
 		fnode->daughters[i] = NULL;
 	}
 	return fnode;
 };
 
-void insertword(struct node *root, string s)
-{
+void insertword(struct node *root, string s) {
 	struct node *temp = root;
-	for(int i = 0; i < s.size(); i++)
-	{
+	for(int i = 0; i < s.size(); i++) {
 		int id = s[i] - 'a';
-		if(temp->daughters[id] == NULL)
-		{
+		if(temp->daughters[id] == NULL) {
 			temp->daughters[id] = makenode();
 			temp->daughters[id]->ch = s[i];
 		}
 		temp = temp->daughters[id];
 	}
-	temp->fin = 1;
+	temp->fin = true;
 }
 
-bool searchword(struct node *root, string s)
-{
+bool searchword(struct node *root, string s) {
 	struct node *temp = root;
-	for(int i = 0; i < s.size(); i++)
-	{
+	for(int i = 0; i < s.size(); i++) {
 		int id = s[i] - 'a';
 		if(temp->daughters[id] == NULL)
-		{
 			return false;
-		}
+		
 		temp = temp->daughters[id];
 	}
-	if(temp->fin == true && temp != NULL)
-	{
+	if(temp->fin == true && temp != NULL) {
 		return true;
 	}
-	else
-	{
-		return false;
-	}
+	return false;
 }
 
-bool del(struct node *cur, string s, int cnt, int length)
-{
-	if(cur)
-	{
-		if(cnt == length)
-		{
-			if(cur->fin)
-			{
+bool del(struct node *cur, string s, int cnt, int length) {
+	if(cur != NULL) {
+		if(cnt == length) {
+			if(cur->fin) {
 				cur->fin = 0;
 				bool flag = 1;
-				for(int i = 0; i < 26; i++)
-				{
+				for(int i = 0; i < 26; i++) {
 					if(cur->daughters[i])
-					{
 						flag = 0;
-					}
 				}
 			}
 		}
-		else
-		{
+		else {
 			int id = s[cnt] - 'a';
-			if(del(cur->daughters[id], s, cnt+1, length))
-			{
+			if(del(cur->daughters[id], s, cnt+1, length)) {
 				free(cur->daughters[id]);
 				cur->daughters[id] = NULL;
 				bool flag = 1;
-				for(int i = 0; i < 26; i++)
-				{
-					if(cur->daughters[i])
-					{
+				for(int i = 0; i < 26; i++) {
+					if(cur->daughters[i]) {
 						flag = 0;
 					}
 				}
@@ -105,11 +81,9 @@ bool del(struct node *cur, string s, int cnt, int length)
 	return 0;
 }
 
-void deleteword(struct node *root, string s)
-{
+void deleteword(struct node *root, string s) {
 	int length = s.size();
-	if(length > 0)
-	{
+	if(length > 0) {
 		del(root, s, 0, length);
 	}
 }
