3c7faaf3b089219efeff513abd70443e754346ca
Madhav Sainanee <30462390+orthodoxparadox@users.noreply.github.com>
2018-12-15 13:01:05 +0530

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -1,44 +1,34 @@
-/*
-Searches for the string w in the string s (of length k). Returns the
-0-based index of the first match (k if no match is found). Algorithm
-runs in O(k) time.
-*/
-
-typedef vector<int> VI;
-
-void buildTable(string& w, VI& t)
-{
-  t = VI(w.length());  
-  int i = 2, j = 0;
-  t[0] = -1;
-  if (w.length() > 1) t[1] = 0;
-  
-  while(i < w.length())
-  {
-    if(w[i-1] == w[j]) { t[i] = j+1; i++; j++; }
-    else if(j > 0) j = t[j];
-    else { t[i] = 0; i++; }
-  }
+void findwin(string pat, int n, int win[]){
+	int s = 0, i = 1;
+	win[s] = 0;
+	while(i < n){
+		if(pat[i] == pat[s]){
+			s++;
+			win[i++] = s;
+		}
+		else{
+			if(s != 0) s = win[s-1];
+			else win[i++] = 0;
+		}
+	}
 }
 
-int KMP(string& s, string& w)
-{
-  int m = 0, i = 0;
-  VI t;
-  
-  buildTable(w, t);  
-  while(m+i < s.length())
-  {
-    if(w[i] == s[m+i])
-    {
-      i++;
-      if(i == w.length()) return m;
-    }
-    else
-    {
-      m += i-t[i];
-      if(i > 0) i = t[i];
-    }
-  }  
-  return s.length();
+void kmpsearch(string full, string pat){
+	int n = full.size(), m = pat.size();
+	int win[m];
+	findwin(pat, m, win);
+	int i = 0, j = 0;
+	while(i < n) {
+		if(pat[j] == full[i]){
+			j++; i++;
+		}
+		if(j == m){
+			cout << i-j << endl;
+			j = win[j-1];
+		}
+		else if (i < n && pat[j] != full[i]){
+			if(j!= 0) j = win[j-1];
+			else i++;
+		}
+	}
 }
