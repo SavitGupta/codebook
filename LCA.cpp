477e955303ec9ffe4d1f76e88dc689d6fd64ee38
Madhav Sainanee <30462390+orthodoxparadox@users.noreply.github.com>
2018-12-15 13:57:54 +0530

    Create LCA.cpp

---
+++ LCA.cpp
@@ -0,0 +1,66 @@
+const int N = 1e5 + 10;
+int par[N][18];
+int h[N];
+vector<int> graph[N];
+
+
+void init(int n)
+{
+	for(int i = 0; i < n + 5; i++)
+	{
+		for(int j = 0; j < 18; j++)
+		{
+			par[i][j] = -1;
+		}
+	}
+}
+
+void dfs(int v, int p = -1)
+{
+	par[v][0] = p;
+	if(p != -1)
+	{
+		h[v] = h[p] + 1;
+	}
+	else
+	{
+		h[v] = 0;
+	}
+	for(int i = 1; i < 18; i++)
+	{
+		if(par[v][i-1] != -1)
+		{
+			par[v][i] = par[par[v][i-1]][i-1];
+		}
+	}
+	for(int i : graph[v])
+	{
+		if(i == p) continue;
+		dfs(i, v);
+	}
+}
+
+int lca(int v, int u)
+{
+	if(h[v] < h[u]) swap(v, u);
+	for(int i = 17; i >= 0; i--)
+	{
+		if(par[v][i] + 1 and h[par[v][i]] >= h[u])
+		{
+			v = par[v][i];
+		}
+	}
+	if(v == u)
+	{
+		return u;
+	}
+	for(int i = 17; i >= 0; i--)
+	{
+		if(par[v][i] != par[u][i])
+		{
+			v = par[v][i];
+			u = par[u][i];
+		}
+	}
+	return par[v][0];
+}
