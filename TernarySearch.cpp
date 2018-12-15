0a88ff1ff921d4eba40ed847f015c2469b8544de
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-15 14:43:00 +0530

    Create TernarySearch.cpp

---
+++ TernarySearch.cpp
@@ -0,0 +1,10 @@
+    // f is bitonic with a single local/global maximum
+    int lo = -1, hi = n;
+    while (hi - lo > 1) {
+        int mid = (hi + lo) / 2;
+        if (f(mid) > f(mid + 1)) 
+            hi = mid;
+        else 
+            lo = mid; 
+    }
+    //lo + 1 is the answer
