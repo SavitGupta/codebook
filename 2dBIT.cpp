3ef5b72c5ef51668d3c96a7a00e86008718da90e
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-16 14:16:11 +0530

    Update 2dBIT.cpp

--- 2dBIT.cpp
+++ 2dBIT.cpp
@@ -1,111 +1,40 @@
-// Constuct2DBIT() to build the 2D-BIT
+const int N = 1000;
+const int M = 1000;
+int BIT[N+1][M+1];  
+int a[N+1][M+1];    
+int n, m;   // 1 <= x <= n, 1 <= y <= m
 
-const int N = 1e5 + 10;
-int BIT[N+1][N+1];
-int mat[N+1][N+1];
-int aux[N+1][N+1];
+// Add val to cell (x, y)
+void update(int x, int y, int val) {
+    for(int x1 = x; x1 <= n; x1 += (x1 & -x1)) {
+        for(int y1 = y; y1 <= m; y1 += (y1 & -y1)) {
+            BIT[x1][y1] += val;
+        }
+    }
+}
 
-// A structure to hold the queries 
-struct Query 
-{ 
-    int x1, y1; // x and y co-ordinates of bottom left 
-    int x2, y2; // x and y co-ordinates of top right 
-}; 
-  
-// A function to update the 2D BIT 
-void updateBIT(int x, int y, int val) 
-{ 
-    for (; x <= N; x += (x & -x)) 
-    { 
-        // This loop update all the 1D BIT inside the 
-        // array of 1D BIT = BIT[x] 
-        for (; y <= N; y += (y & -y)) 
-            BIT[x][y] += val; 
-    } 
-    return; 
-} 
-  
-// A function to get sum from (0, 0) to (x, y) 
-int getSum(int x, int y) 
-{ 
-    int sum = 0; 
-  
-    for(; x > 0; x -= x&-x) 
-    { 
-        // This loop sum through all the 1D BIT 
-        // inside the array of 1D BIT = BIT[x] 
-        for(; y > 0; y -= y&-y) 
-        { 
-            sum += BIT[x][y]; 
-        } 
-    } 
-    return sum; 
-} 
-  
-// A function to create an auxiliary matrix 
-// from the given input matrix. Used only in Construct2DBIT
-void constructAux() 
-{ 
-    // Initialise Auxiliary array to 0 
-    for (int i=0; i<=N; i++) 
-        for (int j=0; j<=N; j++) 
-            aux[i][j] = 0; 
-  
-    // Construct the Auxiliary Matrix 
-    for (int j=1; j<=N; j++) 
-        for (int i=1; i<=N; i++) 
-            aux[i][j] = mat[N-j][i-1]; 
-  
-    return; 
-} 
-  
-// A function to construct a 2D BIT 
-void construct2DBIT() 
-{ 
-    // Create an auxiliary matrix 
-    constructAux(); 
-  
-    // Initialise the BIT to 0 
-    for (int i=1; i<=N; i++) 
-        for (int j=1; j<=N; j++) 
-            BIT[i][j] = 0; 
-  
-    for (int j=1; j<=N; j++) 
-    { 
-        for (int i=1; i<=N; i++) 
-        { 
-            // Creating a 2D-BIT using update function 
-            // everytime we/ encounter a value in the 
-            // input 2D-array 
-            int v1 = getSum(BIT, i, j); 
-            int v2 = getSum(BIT, i, j-1); 
-            int v3 = getSum(BIT, i-1, j-1); 
-            int v4 = getSum(BIT, i-1, j); 
-  
-            // Assigning a value to a particular element 
-            // of 2D BIT 
-            updateBIT(i, j, aux[i][j]-(v1-v2-v4+v3)); 
-        } 
-    } 
-  
-    return; 
-} 
-  
-// A function to answer the queries 
-void answerQueries(Query q[], int m) 
-{ 
-    for (int i=0; i<m; i++) 
-    { 
-        int x1 = q[i].x1 + 1; 
-        int y1 = q[i].y1 + 1; 
-        int x2 = q[i].x2 + 1; 
-        int y2 = q[i].y2 + 1; 
-  
-        int ans = getSum(x2, y2)-getSum(x2, y1-1)- 
-                  getSum(x1-1, y2)+getSum(x1-1, y1-1); 
-  
-        printf ("Query(%d, %d, %d, %d) = %d\n", 
-                q[i].x1, q[i].y1, q[i].x2, q[i].y2, ans); 
-    } 
-    return; 
+// Get sum of (1, 1) to (x, y)
+int get(int x, int y) {
+    int sum = 0;
+    for(int x1 = x; x1 > 0; x1 -= (x1 & -x1)) {
+        for(int y1 = y; y1 > 0; y1 -= (y1 & -y1)) {
+            sum += BIT[x1][y1];
+        }
+    }
+	return sum;
+}
+
+// Get sum of rectangle with lower left cell (x1, y1) to upper right cell (x2, y2)
+// 1 <= x1 <= x2 <= n, 1 <= y1 <= y2 <= m
+int get(int x1, int y1, int x2, int y2) {
+	return get(x2, y2) - get(x2, y1-1) - get(x1-1, y2) + get(x1-1, y1-1);
+}
+
+// Build BIT. (1 based rows and columns)
+void build() {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= m; j++) {
+            update(i, j, a[i][j]);
+        }
+    }
 }
