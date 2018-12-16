3f4c54881f78a1180038dfe3df65927528b0990d
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-16 21:58:06 +0530

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -12,30 +12,24 @@ void init(int n) {
 
 void dfs(int v, int p = -1) {
 	par[v][0] = p;
-	if(p != -1)
-		h[v] = h[p] + 1;
-	else
-		h[v] = 0;
+	if(p != -1) h[v] = h[p] + 1;
+	else h[v] = 0;
 	
-	for(int i = 1; i < K; i++) {
-		if(par[v][i-1] != -1) {
+	for(int i = 1; i < K; i++) 
+		if(par[v][i-1] != -1) 
 			par[v][i] = par[par[v][i-1]][i-1];
-		}
-	}
-	
-	for(int i : graph[v]) {
+
+	for(int i : graph[v]) 
 		if(i != p) 
 			dfs(i, v);
-	}
 }
 
 int lca(int v, int u) {
 	if(h[v] < h[u]) swap(v, u);
-	for(int i = K-1; i >= 0; i--) {
-		if(par[v][i] + 1 and h[par[v][i]] >= h[u]) {
+	
+	for(int i = K-1; i >= 0; i--) 
+		if(par[v][i] + 1 and h[par[v][i]] >= h[u]) 
 			v = par[v][i];
-		}
-	}
 	
 	if(v == u)
 		return u;
