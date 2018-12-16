66268d2a7946bd9393e905530534f54145f79a11
Prakhar Gupta <32891390+prakhar17252@users.noreply.github.com>
2018-12-16 15:35:32 +0530

    Update KruskalMST.cpp

--- KruskalMST.cpp
+++ KruskalMST.cpp
@@ -3,8 +3,7 @@ Kruskal MST
 Initialisation:
 	int n, m;
 	cin >> n >> m;
-	for(int i = 0; i < m; i++)
-	{
+	for(int i = 0; i < m; i++) {
 		int x, y, w;
 		cin >> x >> y >> w;
 		graph.pb(edge(x, y, w));
@@ -14,12 +13,10 @@ About : spanning_tree will store edges
 		mincost will store MST weight
 */
 
-struct edge
-{
+struct edge {
 	int u, v, w;
 	edge(int u, int v, int w): u(u), v(v), w(w) {}
 };
-
 
 const int N = 1e5 + 10;
 vector<edge> graph;
@@ -28,41 +25,34 @@ int mincost = 0;
 int dsu[N];
 int sz[N];
 
-bool edge_sort(const edge &a, const edge &b)
-{
+bool edge_sort(const edge &a, const edge &b) {
 	return a.w < b.w;
 }
 
-int findparent(int u)
-{
+int findparent(int u) {
 	if(dsu[u] == u) return u;
 	return dsu[u] = findparent(dsu[u]);
 }
 
-void merge(edge e)
-{
+void merge(edge e) {
 	int u = e.u;
 	int v = e.v;
 	if(u == v) return; //self loops
-	if(sz[v] > sz[u])
-	{
+	if(sz[v] > sz[u]) {
 		swap(u, v);
 	}
 	dsu[v] = u;
 	sz[u] += sz[v];
 }
 
-void Kruskal(int n)
-{
+void Kruskal(int n) {
 	//1-based
 	sort(all(graph), edge_sort);
-	for(int i = 1; i <= n; i++)
-	{
+	for(int i = 1; i <= n; i++) {
 		dsu[i] = i;
 		sz[i] = 1;
 	}
-	for(edge e : graph)
-	{
+	for(edge e : graph) {
 		int x = findparent(e.u);
 		int y = findparent(e.v);
 		if(x == y) continue;
